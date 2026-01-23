08188283057add1fe44ce484d3c41e1d96861159
Amirul Islam <amirulislamalmamun@gmail.com>
2026-01-23 18:22:26 +0100

    add logging

--- src/core/models.py
+++ src/core/models.py
@@ -1,6 +1,6 @@
 """Data models and result types for SEC EDGAR 10-K fetcher."""
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
 
